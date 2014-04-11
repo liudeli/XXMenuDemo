a64ce434a884c67d2465851941c0838cbb9423a2
shanxu <kingundertree@163.com>
2014-04-11 10:45:59 +0800

    add frame page

---
+++ XXMenuDemo/XXMenuClass/XXRightMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  XXRightMenuViewController.h
+//  XXMenuDemo
+//
+//  Created by shan xu on 14-4-11.
+//  Copyright (c) 2014年 夏至. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XXRightMenuViewController : UIViewController
+
+@end
