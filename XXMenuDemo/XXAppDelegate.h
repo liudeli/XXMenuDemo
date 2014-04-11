c5a9947bb33bd09725596e25cb742f89a39a4127
shanxu <kingundertree@163.com>
2014-04-11 09:00:00 +0800

    新增项目文件

---
+++ XXMenuDemo/XXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XXAppDelegate.h
+//  XXMenuDemo
+//
+//  Created by shan xu on 14-4-11.
+//  Copyright (c) 2014年 夏至. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XXAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XXMenuDemoTests/XXMenuDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  XXMenuDemoTests.m
+//  XXMenuDemoTests
+//
+//  Created by shan xu on 14-4-11.
+//  Copyright (c) 2014年 夏至. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XXMenuDemoTests : XCTestCase
+
+@end
+
+@implementation XXMenuDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
